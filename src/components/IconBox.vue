86a42f6bb1c3539ccf7868f965217302eaaf3595
stevelan <lanyp@qlmtalent.com>
2020-05-28 13:49:57 +0800

    Added IconBox component

---
+++ src/components/IconBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="box" v-on:click="navigateToPath">
+    <slot></slot>
+    <h3 class="name">{{ boxTitle }}</h3>
+    <p class="description">{{ description }}</p>
+    <router-link class="learn-more" :to="{path: this.navigatePath}">
+      查看详情
+      <b-icon icon="chevron-double-right"></b-icon>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/Features-Boxed.css'
+
+export default {
+  name: 'IconBox',
+  props: {
+    boxTitle: String,
+    description: String,
+    navigatePath: String
+  },
+  methods: {
+    navigateToPath: function () {
+      if (this.navigatePath.includes('http') || this.navigatePath.includes('www.')) {
+        window.location = this.navigatePath
+      } else {
+        this.$router.push({ path: this.navigatePath })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.learn-more
+  font-family: Helvetica
+</style>
