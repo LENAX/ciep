0e9e80b58a05c9efaab5985059c1fc3655fd7b7c
stevelan <lanyp@qlmtalent.com>
2020-05-28 13:49:31 +0800

    Import fontawesome

--- src/main.js
+++ src/main.js
@@ -8,13 +8,21 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { 
+  faCalendarPlus, faCogs, faMicrochip,
+  faRobot, faFlask, faBuilding
+} from '@fortawesome/free-solid-svg-icons'
 
-// library.add(faUserSecret)
+library.add(faCalendarPlus)
+library.add(faCogs)
+library.add(faMicrochip)
+library.add(faRobot)
+library.add(faFlask)
+library.add(faBuilding)
 
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
